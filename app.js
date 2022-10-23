9c576eabf9f240156827b41f9e62ed5860442b8f
findfrankie <106564473+findfrankie@users.noreply.github.com>
2022-10-23 18:41:52 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+// var cardButton = document.getElementById ("cardButton");
+// var payment = document.getElementById ("myPayment");
+// var myClose = document.getElementById ("myClose");
+
+// cardButton.onclick = function() {
+//     payment.style.display = "block";
+//   }
+  
+//   // When the user clicks on <span> (x), close the modal
+//   myClose.onclick = function() {
+//     modal.style.display = "none";
+//   }
+  
+//   // When the user clicks anywhere outside of the modal, close it
+//   window.onclick = function(event) {
+//     if (event.target == payment) {
+//       payment.style.display = "none";
+//     }
+//   }
+
+
+// function myFunction() {
+//     document.getElementById("myPayment").style.display = "flex";
+//   }
+
+
+const myPayment =document.querySelector(".myPayment");
+
+function myFunction(){
+   myPayment.style.display = "flex";
+}
+
+
+
+
+// const cardButton   = document.querySelector(".cardButton");
+// const payment = document.querySelector(".payment");
+// const close = document.querySelector(".close");
+
+//  cardButton .addEventListener ("click",()=>{
+//     payment.style.display ="flex"
+// })
+
+// close .addEventListener ("click",()=>{
+//     payment.style.display ="none"
+// })
